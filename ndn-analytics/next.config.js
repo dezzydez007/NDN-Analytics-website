be75c82123cb7d0af9235aa904437773592e4679
dezzydez007 <nkefuan@yahoo.com>
2023-06-30 00:11:39 +0000

    configuring the right entry point into the app.

--- ndn-analytics/next.config.js
+++ ndn-analytics/next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    // The entry file for your application.
+    // This file will be used to bootstrap your application.
+    entry: './src/_app.js',
+  };
+  
+  module.exports = nextConfig;
+  
